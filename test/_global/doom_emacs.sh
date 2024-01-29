7ce10fdfcd3d130bc952e5045171b4c0910b8d3e
Valery <valery.lavrentiev@onefootball.com>
2024-01-29 19:58:26 +0100

    fix: added sample test

--- test/_global/doom_emacs.sh
+++ test/_global/doom_emacs.sh
@@ -3,7 +3,7 @@
 # The 'test/_global' folder is a special test folder that is not tied to a single feature.
 #
 # This test file is executed against a running container constructed
-# from the value of 'color_and_hello' in the tests/_global/scenarios.json file.
+# from the value of 'doom-emacs' in the tests/_global/scenarios.json file.
 #
 # The value of a scenarios element is any properties available in the 'devcontainer.json'.
 # Scenarios are useful for testing specific options in a feature, or to test a combination of features.
@@ -16,16 +16,13 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-echo -e "The result of the 'color' command will be:\n"
-color
-echo -e "The result of the 'hello' command will be:\n"
-hello
+echo -e "The result of the 'doom-emacs' command will be:\n"
+emacs --version
 echo -e "\n"
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "check purple is my favorite color" bash -c "color | grep 'my favorite color is purple'"
-check "check I am greeting with 'Greetings'" bash -c "hello | grep 'Greetings, $(whoami)'"
+# check "check purple is my favorite color" bash -c "color | grep 'my favorite color is purple'"
 
 
 # Report result
